6ee9c6113d44990b78a2a7250e1a6bec9cd99dcb
Aurimas Liutikas <aurimas@liutikas.net>
2020-11-15 13:12:38 -0800

    Upgrade Gradle and Kotlin to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
     application
-    kotlin("kapt") version "1.3.72"
+    kotlin("kapt") version "1.4.10"
 }
 
 repositories {
@@ -11,14 +11,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.72")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
     implementation("ru.gildor.coroutines:kotlin-coroutines-okhttp:1.0")
     implementation("com.squareup.okhttp3:okhttp:4.7.2")
-    implementation("com.squareup.moshi:moshi:1.8.0")
+    implementation("com.squareup.moshi:moshi:1.11.0")
     implementation("org.apache.commons:commons-csv:1.8")
     implementation("scientifik:plotlykt-core:0.1.2")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
 }
 
 application {
